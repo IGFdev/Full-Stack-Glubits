05176f5a7d9cc27e38a1f7f55dcb845726c19380
Ignacio <ignaciogf75@hotmail.com>
2022-11-16 18:13:06 -0300

    clase 2/11

--- public/js/app.js
+++ public/js/app.js
@@ -1,49 +1,38 @@
-const boton = document.getElementById('mostrar-btn');
+const formulario = document.getElementById('login-form');
+const nombreTxt = document.getElementById('nombreH1');
+const passwordTxt = document.getElementById('passwordH2');
+const emailTxt = document.getElementById('emailH3');
+const nombreError = document.getElementById('nombreError');
 
-boton.addEventListener('click', () => {
-    const secretoH1 = document.getElementById('secreto');
-
-    secretoH1.classList.toggle('oculto');
-
-    if (secretoH1.classList.contains('oculto')) {
-        boton.innerText = 'Mostrar secreto';
-    } else {
-        boton.innerText = 'Ocultar secreto';
-    }
-
-    //boton.innerText = secretoH1.classList.contains('oculto') ? 'Mostrar secreto' : 'Ocultar secreto';
-});
-
-boton.addEventListener('mouseenter', (e) => {
-    e.target.style.backgroundColor = 'red';
-});
-
-boton.addEventListener('mouseout', (e) => {
-    e.target.style.backgroundColor = 'whitesmoke';
-});
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    alert('sitio cargado correctamente');
-});
-
-document.addEventListener('keydown', (e) => {
-    console.log(e.key)
-
-    if (e.key === 'a') {
-        const secretoH1 = document.getElementById('secreto');
-
-        secretoH1.classList.toggle('oculto');
-
-        if (secretoH1.classList.contains('oculto')) {
-            boton.innerText = 'Mostrar secreto';
-        } else {
-            boton.innerText = 'Ocultar secreto';
-        }
-    }
-});
-
-document.addEventListener('contextmenu', (e) => {
+formulario.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log('hola')
+
+    nombreTxt.innerText = formulario.nombre.value;
+    passwordTxt.innerText = formulario.password.value;
+    emailTxt.innerText = formulario.email.value;
+});
+
+formulario.nombre.addEventListener('focus', () => {
+    console.log('Se clickea el input')
 })
+
+formulario.nombre.addEventListener('blur', () => {
+    console.log('Se desclickea el input')
+})
+
+/* formulario.nombre.addEventListener('change', (e) => {
+    if (formulario.nombre.value.length < 8) {
+        nombreError.innerText = 'El nombre debe contener al menos 8 caracteres';
+    } else {
+        nombreError.innerText = '';
+    }
+}); */
+
+formulario.nombre.addEventListener('input', e => {
+    console.log(e.target.value);
+    if (formulario.nombre.value.length < 8) {
+        nombreError.innerText = 'El nombre debe contener al menos 8 caracteres';
+    } else {
+        nombreError.innerText = '';
+    }
+});
